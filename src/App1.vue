2428abd88fa2925c0efc837a45d7686ba729f432
Dalibor <dalibor.gc@gmail.com>
2017-11-14 00:44:33 +0100

    Added comments

--- src/App1.vue
+++ src/App1.vue
@@ -3,7 +3,7 @@
       <h2>{{title}}</h2>
       <p>{{greet('Dali')}}</p>
     
-    <peoples></peoples>
+    <peoples></peoples> <!-- Global component -->
     <people-local></people-local>
   </div>
 </template>
@@ -31,7 +31,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped> /*Css samo za ovaj comenent*/
   
 </style>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App1.vue'
 import MyApp from './MyApp.vue'
 
-Vue.component('peoples', MyApp);
+Vue.component('peoples', MyApp); //Global component
 
 new Vue({
   el: '#app',
