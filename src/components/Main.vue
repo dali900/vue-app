737f36a876b224ecc8561b80cc9dbcf466bb76ae
dali900 <daliibore@gmail.com>
2017-11-15 23:36:16 +0100

    Slots

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,11 +7,26 @@
        <h3 v-show="person.show">{{person.email}}</h3>
      </li>
    </ul>
+
+   <form-helper>
+      <div slot="form-header">
+          <h3>This is the title of a form</h3>
+          <p>This is some info about the form</p>
+      </div>
+      <div slot="form-fields">
+          <input type="text" placeholder="name" required />
+          <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+          <button v-on:click="handleSubmit">Submit</button>
+      </div>
+  </form-helper>
  </div>
  
 </template>
 
 <script>
+import formHelper from './formHelper.vue'
 
 export default {
   props: ["persons"], //Prihvatanje podataka iz root komponete MyApp, normalan access za methods
@@ -22,10 +37,18 @@ export default {
       required: true
     }
   },*/
+  components: {
+        'form-helper': formHelper
+    },
   data () {
     return {
       
     }
+  },
+  methods: {
+      handleSubmit: function(){
+          alert('thanks for submitting');
+      }
   }
 
 }
