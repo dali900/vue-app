3a856eb0660f7f9e7593f900cee60cac7722a373
Dalibor <dalibor.gc@gmail.com>
2017-11-21 00:20:53 +0100

    Blog input binding

---
+++ src/BlogApp.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <add-blog></add-blog>
+    </div>
+</template>
+
+<script>
+// Imports
+import addBlog from './blog_comp/addBlog.vue';
+export default {
+    components: {
+        'add-blog': addBlog
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Arial';
+}
+</style>
